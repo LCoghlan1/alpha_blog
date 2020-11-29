7cfcb48df9a8f1e8dea0db65654ac8d7d146ad99
LCoghlan1 <ubuntu@ip-172-31-2-76.ec2.internal>
2020-11-29 23:02:12 +0000

    add bootstrap styling to all pages

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#removes div that errors are displayed in - this was causing bootstrap formatting to push out
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+    html_tag.html_safe
+end
