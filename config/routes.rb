f2454349f1493b99ea4d659d97dba13f940cf545
LCoghlan1 <ubuntu@ip-172-31-2-76.ec2.internal>
2020-12-04 16:43:18 +0000

    Changed primary button colour

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
   
   resources :articles , only: [:show, :index, :new, :create, :edit, :update, :destroy] #all routes available so 'only' not needed 
   
+  get 'signup', to: 'users#new'
+  
+  post 'users', to: 'users#create'
+  
+  resources :users, except: [:new]
   
   
 end
