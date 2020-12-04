9af8f0e0b98994340c1db6d11c80c8df187f7308
LCoghlan1 <ubuntu@ip-172-31-2-76.ec2.internal>
2020-12-04 17:10:57 +0000

    create users create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,22 @@ class UsersController < ApplicationController
    def new
        @user = User.new
    end
+   
+   def create
+       @user = User.new(user_params)
+       #if statement for no user validation errors
+       if @user.save
+           flash[:notice] = "Welcome to Luke's Blog #{@user.username}, you're signed up n' stuff. Go git t'articlin"
+           redirect_to articles_path
+       else
+           render 'new'
+       end
+   end
     
+end
+
+private
+
+def user_params
+   params.require(:user).permit(:username, :email, :password)
 end
