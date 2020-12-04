accce7dc5e9f6ab4dab2168d5d1431c36039c0e3
LCoghlan1 <ubuntu@ip-172-31-2-76.ec2.internal>
2020-12-04 13:51:10 +0000

    add one to many assoc. between users and articles from back end

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ApplicationRecord
+    
+    #create one to many relationship with users (it belongs to users)
+    belongs_to :user
+    
     validates :title, presence: true, length: { minimum: 3, maximum: 100 }
     validates :description, presence: true, length: {minimum: 3, maximum: 500 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
     
+    #create a one to many relationship between articles
+    has_many :articles
+    
     #ensure username is present, unique (incl. case sensitivity) and of certain length before a user object is saved to db
     validates :username, presence: true, uniqueness: { case_sensitive: false }, length: {minimum:3, maximum: 25}
     
