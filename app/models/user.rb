220a535bb8b6ab20497c64410d73ba0fdb8e0e33
LCoghlan1 <ubuntu@ip-172-31-2-76.ec2.internal>
2020-12-04 13:29:53 +0000

    Add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
     
+    #ensure username is present, unique (incl. case sensitivity) and of certain length before a user object is saved to db
+    validates :username, presence: true, uniqueness: { case_sensitive: false }, length: {minimum:3, maximum: 25}
+    
+    #regex saved as constant as will not change
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    
+    validates :email, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 105}, format: { with: VALID_EMAIL_REGEX }
+    
 end
